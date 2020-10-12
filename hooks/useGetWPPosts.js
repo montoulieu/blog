5dba700ebc58b071eb54fb2194220702f12f9008
Pieter Montoulieu <pieter@montoulieu.dev>
2020-10-12 17:42:45 -0600

    Cleanup

--- hooks/useGetWPPosts.js
+++ hooks/useGetWPPosts.js
@@ -1,6 +1,5 @@
 import axios from "axios"
 import useBlogStore from '../hooks/useBlogStore';
-import { useEffect } from 'react';
 
 const importBlogPosts = async () => {
   const apiUrl = `${process.env.NEXT_PUBLIC_WP_API_URL}/wp/v2/posts?per_page=100&_fields=date,excerpt,title,slug,content`
