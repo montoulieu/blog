cd22ffdb5d1679f082e8ee773e392f5398f65bf8
Pieter Montoulieu <pieter@montoulieu.dev>
2020-10-16 15:12:45 -0600

    Adding button link to purgecss whitelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  purge: {
+    content: [
+      './pages/**/*.{js,jsx,ts,tsx}',
+      './components/**/*.{js,jsx,ts,tsx}',
+    ],
+    options: {
+      whitelist: ['button-link']
+    },
+  },
   theme: {
     fontFamily: {
       'body': ['Noto Sans JP', 'Arial', 'sans-serif'],
